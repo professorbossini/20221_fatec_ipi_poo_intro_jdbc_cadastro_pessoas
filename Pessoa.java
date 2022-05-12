49526aaf604c9da098ff0d70d27a078cd1fe4a29
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-12 17:58:07 -0300

    apostila 09: implementacao do metodo inserir pessoa da listagem 2.12.2

--- Pessoa.java
+++ Pessoa.java
@@ -1,8 +1,29 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 public class Pessoa {
     private int codigo;
     private String nome;
     private String fone;
     private String email;
+
+    public void inserir() throws Exception{
+        //1. Definir o comando SQL
+        String sql = "INSERT INTO tb_pessoa (nome, fone, email) VALUES (?, ?, ?)";
+        //2. Abrir uma conexão com o MySQL Server
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection conexao = factory.getConnection();
+        //3. Preparar o comando (solicitar ao MySQL Server que compile o comando SQL previamente)
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        //4. Substituir os eventuais placeholders
+        ps.setString(1, nome);
+        ps.setString(2, fone);
+        ps.setString(3, email);
+        //5. Executar o comando
+        ps.execute();
+        //6. Fechar os recursos (conexão e o comando preparado)
+        ps.close();
+        conexao.close();
+    }
 
     public int getCodigo() {
         return codigo;
@@ -26,7 +47,7 @@ public class Pessoa {
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     public void setFone(String fone) {
         this.fone = fone;
     }
