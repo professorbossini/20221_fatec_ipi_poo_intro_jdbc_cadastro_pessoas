08b12b06d16098f3ad1e27efd48dbcb1a0227efe
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-12 18:09:32 -0300

    apostila 09: fim da listagem 2.12.3

--- Principal.java
+++ Principal.java
@@ -3,22 +3,33 @@ import javax.swing.JOptionPane;
 public class Principal {
    public static void main(String[] args) {
        String menu = "1-Cadastrar\n2-Atualizar\n3-Apagar\n4-Listar\n0-Sair";
-       int op;
+       int op = 0;
        do{
-           op = Integer.parseInt(JOptionPane.showInputDialog(menu));
-           switch (op){
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 0:
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Opção inválida");
+           try{
+               op = Integer.parseInt(JOptionPane.showInputDialog(menu));
+                switch (op){
+                    case 1:
+                        String nome = JOptionPane.showInputDialog("Digite o nome");
+                        String fone = JOptionPane.showInputDialog("Digite o fone");
+                        String email = JOptionPane.showInputDialog("Digite o e-mail");
+                        Pessoa p = new Pessoa(nome, fone, email);
+                        p.inserir();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opção inválida");
+               }
+           }
+           catch (Exception e){
+                JOptionPane.showMessageDialog(null, "Falhou...");
+                System.out.println(e.getMessage());
            }
 
        }while (op != 0);
